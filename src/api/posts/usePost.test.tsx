131de2ab8c199d92690a70d8447d120e1d3051d7
Babak Mehrabi <b.mehrabi.91@gmail.com>
2025-05-22 19:46:02 +0200

    Adding test for usePost hook

---
+++ src/api/posts/usePost.test.tsx
@@ -0,0 +1,37 @@
+import { PostType } from '@app/api/models/Post';
+import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactElement, ReactNode } from 'react';
+import { renderHook, waitFor } from '@testing-library/react';
+import usePost from '@app/api/posts/usePost';
+
+vi.mock('axios');
+
+describe('usePost', () => {
+  const mockedPost: PostType = {
+    id: 1,
+    title: 'title',
+    description: 'description',
+    createdAt: '',
+    author: {
+      avatarUrl: '',
+      name: 'Babak',
+    },
+    image: {},
+    publishedDate: '',
+  };
+  const queryClient = new QueryClient();
+  const wrapper = ({ children }: { children: ReactNode }): ReactElement => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+
+  it('returns the post by id from API', async () => {
+    vi.mocked(axios.get).mockResolvedValue({ data: mockedPost });
+
+    const { result } = renderHook(() => usePost(1), { wrapper });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(result.current.data).toEqual(mockedPost);
+  });
+});
