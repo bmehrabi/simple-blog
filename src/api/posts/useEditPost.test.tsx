67dec60fbed1502f61c361f0b867024d437e781f
Babak Mehrabi <b.mehrabi.91@gmail.com>
2025-05-23 10:01:07 +0200

    Adding test for useEditPost

---
+++ src/api/posts/useEditPost.test.tsx
@@ -0,0 +1,63 @@
+import { PostType } from '@app/api/models/Post';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactElement, ReactNode } from 'react';
+import { renderHook, waitFor } from '@testing-library/react';
+import * as apis from '@app/api/posts/apis';
+import queryClientModule from '@app/api/queryClient';
+import useEditPost from '@app/api/posts/useEditPost';
+
+describe('useEditPost', () => {
+  const mockedPost: PostType = {
+    id: 1,
+    title: 'title',
+    description: 'description',
+    createdAt: '',
+    author: {
+      avatarUrl: '',
+      name: 'Babak',
+    },
+    image: {},
+    publishedDate: '',
+  };
+  const queryClient = new QueryClient();
+  const wrapper = ({ children }: { children: ReactNode }): ReactElement => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+
+  it('calls patchPost when mutate is called', async () => {
+    const mockPostData = new FormData();
+    mockPostData.append('title', 'title');
+    mockPostData.append('description', 'description');
+
+    const patchPostSpy = vi.spyOn(apis, 'patchPost').mockResolvedValue(mockedPost);
+
+    const { result } = renderHook(() => useEditPost(), { wrapper });
+
+    result.current.mutate({ id: 1, formData: mockPostData });
+
+    await waitFor(() => {
+      expect(patchPostSpy).toHaveBeenCalledTimes(1);
+      expect(patchPostSpy).toHaveBeenCalledWith({ id: 1, formData: mockPostData });
+    });
+  });
+
+  it('invalidate queries on success', async () => {
+    const mockPostData = new FormData();
+    mockPostData.append('title', 'title');
+    mockPostData.append('description', 'description');
+
+    const patchPostSpy = vi.spyOn(apis, 'patchPost').mockResolvedValue(mockedPost);
+    const invalidateQueriesSpy = vi.spyOn(queryClientModule, 'invalidateQueries');
+
+    const { result } = renderHook(() => useEditPost(), { wrapper });
+
+    result.current.mutate({ id: 1, formData: mockPostData });
+
+    await waitFor(() => {
+      expect(patchPostSpy).toHaveBeenCalledTimes(1);
+      expect(invalidateQueriesSpy).toHaveBeenCalledTimes(2);
+      expect(invalidateQueriesSpy).toHaveBeenCalledWith({ queryKey: ['POSTS'] });
+      expect(invalidateQueriesSpy).toHaveBeenCalledWith({ queryKey: ['POSTS', 1] });
+    });
+  });
+});
